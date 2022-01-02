9cea469266af9b416d33b523c7a18baa80c2537c
VapoR <ryvapor@gmail.com>
2022-01-01 20:27:44 -0500

    Unneccesary null checks

--- src/main/java/com/github/vaporrrr/btediscordaddon/LP.java
+++ src/main/java/com/github/vaporrrr/btediscordaddon/LP.java
@@ -5,28 +5,20 @@ import net.luckperms.api.LuckPermsProvider;
 import net.luckperms.api.model.group.Group;
 import net.luckperms.api.node.matcher.NodeMatcher;
 import net.luckperms.api.node.types.InheritanceNode;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.RegisteredServiceProvider;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Set;
 
 public class LP {
-    private LuckPerms luckPerms;
+    private final LuckPerms luckPerms;
 
     public LP() {
-        RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
-        if (provider != null) {
-            luckPerms = provider.getProvider();
-        }
+        luckPerms = LuckPermsProvider.get();
     }
 
     public int getGroupSize(String groupName) {
-        if (luckPerms == null) {
-            luckPerms = LuckPermsProvider.get();
-        }
         Group group = luckPerms.getGroupManager().getGroup(groupName);
         if (group == null) return -1;
         NodeMatcher<InheritanceNode> matcher = NodeMatcher.key(InheritanceNode.builder(group).build());
@@ -34,9 +26,6 @@ public class LP {
     }
 
     public String getPlayerGroup(Player player) {
-        if (luckPerms == null) {
-            luckPerms = LuckPermsProvider.get();
-        }
         ArrayList<Group> groups = allGroups();
         Collections.reverse(groups);
         for (Group group : groups) {
